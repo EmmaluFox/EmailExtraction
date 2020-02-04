0cb3b88433506363ae1e48f275a5c7d1d7153c5c
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-04 11:21:35 +0000

    Naive Approach

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.Mime;
 
 namespace Email_Extraction
 {
@@ -6,7 +8,22 @@ namespace Email_Extraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DomainSearch());
+            
+            static int DomainSearch()
+            {
+                int counter = 0;
+                string input = File.ReadAllText(@"C:\Training\Email Extraction\Email Extraction\text.txt");
+               
+                for (int i = 0; i < (input.Length-13); i++)
+                {
+                    if (input.Substring(i, 13) == "@softwire.com")
+                    {
+                        counter += 1;
+                    }
+                }
+                return counter;
+            }
         }
     }
 }
